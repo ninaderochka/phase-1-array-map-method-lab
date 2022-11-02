e4fd0f2fbc86dd45ad862569514024bb10967892
Nina Hawari <nina.hawari@gmail.com>
2022-11-02 23:12:59 +0200

    Completed Assignment

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+
+// const tutorialsTitle = tutorials.toString();
+
+const titleCased = () => { 
+  return tutorials.map((line) => {
+      const capital = line.split(' ').map((word) => {
+            return word.charAt(0).toUpperCase() + word.slice(1);
+            }).join(' ');
+
+
+            return capital;
+            
+  }
+ 
+
+
+
+
+
+)}
+
+
+
+
+titleCased()
+console.log(titleCased(tutorials))
+
+
